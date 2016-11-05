63697b0f3cf509f45ffdcde79d744e3be942cdb8
Eric Balawejder <eric.balawejder@gmail.com>
2016-11-05 18:30:33 -0400

    created removeCPTable() function

--- src/edu/piechart/LearnPieChartNet.java
+++ src/edu/piechart/LearnPieChartNet.java
@@ -8,28 +8,24 @@ public class LearnPieChartNet
 	{
 		try 
 		{
+			// Environ = null for unregister Netica usage
 			Environ env = new Environ(null);
 
 			// Use if case file is comma-delimited.
 			env.setCaseFileDelimChar(',');
-
+			
+			// file path of Netica .dne net file 
+			String filePath = "piechart_bayes_data/NetFiles/pieEvidence.dne";
+			
 			// Read in the net created by PieChartNet.java
-			Net net = new Net(new Streamer("piechart_bayes_data/NetFiles/pieEvidence.dne"));
-			NodeList nodes = net.getNodes();
-			int numberOfNodes = nodes.size();
-
-			// Remove CPTables of nodes in net, so new ones can be learned.
-			for (int n = 0; n < numberOfNodes; n++) 
-			{
-				Node node = (Node) nodes.get(n);
-				node.deleteTables();
-			}
-
-			// System.out.println(nodes);
-
+			Net net = new Net(new Streamer(filePath));
+			
+			// function to remove CPTables of nodes in net, so new ones can be learned.
+			removeCPTables(filePath);
+			
 			// Read in the case file pieEvidence.cas and learn new CPTables.
 			Streamer caseFile = new Streamer("piechart_bayes_data/DataFiles/pieEvidenceCSV.cas");
-			net.reviseCPTsByCaseFile(caseFile, nodes, 1.0);
+			net.reviseCPTsByCaseFile(caseFile, net.getNodes(), 1.0);
 
 			// Builds the junction tree of cliques (maximal complete subgraph).
 			net.compile();
@@ -49,18 +45,40 @@ public class LearnPieChartNet
 			e.printStackTrace();
 		}
 	}
+	
+	// function to remove the Conditional Probability Tables from a net
+	public static void removeCPTables(String filename) throws NeticaException
+	{
+		Net net = new Net(new Streamer(filename));
+		NodeList nodes = net.getNodes();
+		int numberOfNodes = nodes.size();
+
+		// Remove CPTables of nodes in net, so new ones can be learned.
+		for (int n = 0; n < numberOfNodes; n++) 
+		{
+			Node node = (Node) nodes.get(n);
+			node.deleteTables();
+		}
+	}
 }
 
 /*
- * ============================================================== This alternate
- * way can replace the net.reviseCPTsByCaseFile line above, if you need to
- * filter or adjust individual cases.
+ * ============================================================== 
+ * This alternate way can replace the net.reviseCPTsByCaseFile line above, 
+ * if you need to filter or adjust individual cases.
  * 
- * long[] casePosn = new long[1]; casePosn[0] = Net.FIRST_CASE; while (true) {
- * net.retractFindings(); net.readCase (casePosn, caseFile, nodes, null, null);
- * if (casePosn[0] == Net.NO_MORE_CASES) break;
- * 
- * net.reviseCPTsByFindings (nodes, 1.0); casePosn[0] = Net.NEXT_CASE; }
+ * long[] casePosition = new long[1]; 
+ * casePosition[0] = Net.FIRST_CASE; 
+ * while (true) 
+ * {
+ * 		net.retractFindings(); 
+ * 		net.readCase (casePosition, caseFile, nodes, null, null);
+ * 		if (casePosition[0] == Net.NO_MORE_CASES) 
+ * 		{
+ * 			break;
+ * 		}
+ * net.reviseCPTsByFindings (nodes, 1.0); casePosition[0] = Net.NEXT_CASE; 
+ * }
  * ==============================================================
  */
 
